6570bd8fc80b0da5ecac8e48401120fac488e0e7
Kamila <kamilla.adamson@gmail.com>
2023-01-20 02:19:31 +0600

    Добавила задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+
+if (num1 > num2 && num1 > num3) 
+{
+  Console.Write("max = " + num1);
+} 
+
+if (num2 > num1 && num2 > num3) 
+{ 
+  Console.Write("max = " + num2);
+} 
+
+if (num3 > num1 && num3 > num2) 
+{
+  Console.Write("max = " + num3);
+}
+
+if (num1 == num2 && num1 == num3)
+{
+   Console.Write("значения равны");
+}
+
+
+
+
+
+
+
+
