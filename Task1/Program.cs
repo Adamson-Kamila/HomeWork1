c467ee6352ac11fbeabfe587386b1f40632f1578
Kamila <kamilla.adamson@gmail.com>
2023-01-20 01:22:54 +0600

    добавила задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numa = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int numb = int.Parse(Console.ReadLine()!);
+// int numa = 9;
+// int numb = 7;
+if (numa > numb) 
+{
+  Console.Write("max = " + numa);
+} else if (numa < numb) 
+{ 
+  Console.Write("max = " + numb);
+} else Console.Write("значения равны");
